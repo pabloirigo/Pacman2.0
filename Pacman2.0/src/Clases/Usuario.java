0ba4afc0516080e54cc0d71c675379e770427493
Pablo <Pablo@10.164.48.31>
2017-09-11 12:35:09 +0200

    Clase Usuario

---
+++ Pacman2.0/src/Clases/Usuario.java
@@ -0,0 +1,34 @@
+package Clases;
+
+public class Usuario {
+	private int puntuacion;
+	private String nombre;
+	
+	public Usuario(int puntuacion, String nombre) {
+		this.puntuacion = puntuacion;
+		this.nombre = nombre;
+	}
+	
+	public Usuario(){
+		
+	}
+
+	public int getPuntuacion() {
+		return puntuacion;
+	}
+
+	public void setPuntuacion(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+}
