219391c0ddf2a642484f24af89652e60741dbd78
Pablo Emilio Escobar <Pablo Emilio Escobar@Luis-PC>
2017-10-19 22:23:32 +0200

    botones wasd y flechas listener

--- Pacman2.0/src/TiposDeDatos/Icono.java
+++ Pacman2.0/src/TiposDeDatos/Icono.java
@@ -6,46 +6,56 @@ import java.awt.event.KeyListener;
 
 
 public class Icono  implements KeyListener {
-	private int x=0;
-	private int y=0;
-	private int dx=0;
-	private int dy=0;
-	public Icono(){
-		
 	
-		
+	public Icono(){
+
+
+
 	}
 	public void mover() {
-		x = dx;
-		y +=dy;
+		
 	}
-	
-	
-/*esta mal, tengo que pensarlo y no me da tiempo a mas */
-	
+
+
+	/*esta mal, tengo que pensarlo y no me da tiempo a mas */
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		int key = e.getKeyCode();
-		
-		if(key == KeyEvent.VK_LEFT) {
-			dx = -1;
-			
+		int keyCode = e.getKeyCode();
+		switch( keyCode ) { 
+		case KeyEvent.VK_UP:
+	//	Pacman pacman = new Pacman();
+			break;	
+		case KeyEvent.VK_DOWN:	
+			break;
+		case KeyEvent.VK_LEFT:
+			break;
+		case KeyEvent.VK_RIGHT :
+			break;
+		case KeyEvent.VK_W:
+			break;			
+		case KeyEvent.VK_S:		
+			break;
+		case KeyEvent.VK_A:		
+			break;			
+		case KeyEvent.VK_D :			
+			break;
 		}
-		//if(key == KeyEvent.VK_RIGHT)
 	}
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
 
-}
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+
+	}
--- Pacman2.0/src/TiposDeDatos/Pacman.java
+++ Pacman2.0/src/TiposDeDatos/Pacman.java
@@ -2,7 +2,16 @@ package TiposDeDatos;
 
 public class Pacman extends Icono {
 
+	private int x=0;
+	private int y=0;
+	private int dx=0;
+	private int dy=0;	
 	
-	
+	public Pacman() {
+		
+		
+		
+		
+	}
 	
 }
